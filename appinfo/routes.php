9fa72746d622dc9c0aeaf001f674303f5ecefd60
xujaan <ahmadakhussyuj.an@gmail.com>
2026-01-20 01:29:42 +0700

    refactor: Standardize route controller capitalization and simplify Application constructor.

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -1,11 +1,11 @@
 <?php
 return [
     'routes' => [
-        ['name' => 'page#index', 'url' => '/', 'verb' => 'GET'],
-        ['name' => 'api#listAssets', 'url' => '/api/list', 'verb' => 'GET'],
-        ['name' => 'api#getFiles', 'url' => '/api/files', 'verb' => 'GET'],
-        ['name' => 'api#createFolder', 'url' => '/api/folder', 'verb' => 'POST'],
-        ['name' => 'api#uploadFile', 'url' => '/api/upload', 'verb' => 'POST'],
-        ['name' => 'api#downloadPackage', 'url' => '/api/package', 'verb' => 'GET'],
+        ['name' => 'Page#index', 'url' => '/', 'verb' => 'GET'],
+        ['name' => 'Api#listAssets', 'url' => '/api/list', 'verb' => 'GET'],
+        ['name' => 'Api#getFiles', 'url' => '/api/files', 'verb' => 'GET'],
+        ['name' => 'Api#createFolder', 'url' => '/api/folder', 'verb' => 'POST'],
+        ['name' => 'Api#uploadFile', 'url' => '/api/upload', 'verb' => 'POST'],
+        ['name' => 'Api#downloadPackage', 'url' => '/api/package', 'verb' => 'GET'],
     ]
 ];
--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -9,8 +9,8 @@ use OCP\AppFramework\Bootstrap\IRegistrationContext;
 class Application extends App implements IBootstrap {
     public const APP_ID = 'nextcloud-microstock-dam';
 
-    public function __construct(array $urlParams = []) {
-        parent::__construct(self::APP_ID, $urlParams);
+    public function __construct() {
+        parent::__construct(self::APP_ID);
     }
 
     public function register(IRegistrationContext $context): void {
